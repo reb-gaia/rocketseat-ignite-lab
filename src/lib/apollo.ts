08ae64335deab1e29ba9559b77a30dac885847ce
Rebeca Gaia <reb.gaia@gmail.com>
2022-07-27 10:29:18 -0300

    Criando página de Incrição

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri:'https://api-sa-east-1.hygraph.com/v2/cl612ifnj4h4f01ut9nkc4erj/master',
+  uri:import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
